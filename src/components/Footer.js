bdad9601c519efa7bd1deadbc97afecb6ce60160
Rachel Miller <millerrach@gmail.com>
2017-02-28 19:27:56 +0000

    Fixed link issue with inserting z-index

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../style/css/footer.css';
 export default () => {
     return (
         <div id="footerContainer">
-                <img src={require(`../images/airplane.svg`)} height="80" width="80" alt="icon" /><br/>
+                <img src={require(`../images/airplane.svg`)} height="80" width="80" alt="icon" />
                 <p>Made by<a href="https://geometrics.io" target="_blank"> geometrics.io</a></p>
         </div>
     )
