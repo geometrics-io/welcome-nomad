5f4090dbe25068002ee23ebd334716ef2e9ca482
Rachel Miller <millerrach@gmail.com>
2017-02-28 19:21:45 +0000

    Animation for country of origin info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ import '../style/css/footer.css';
 export default () => {
     return (
         <div id="footerContainer">
-            <div className="footerItem">
                 <img src={require(`../images/airplane.svg`)} height="80" width="80" alt="icon" /><br/>
-                <p>Made by<a href="geometrics.io"> geometrics.io</a></p>
-            </div>
+                <p>Made by<a href="https://geometrics.io" target="_blank"> geometrics.io</a></p>
         </div>
     )
 }
--- src/components/Origin.js
+++ src/components/Origin.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import '../style/css/origin.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-export default () => {
-    return (
-        <div id="origin">
-            <p>USA is currently the country of origin. <span className="clickable">Learn more...</span></p>
-            <p>If you would like another country added, <a href="mailto:mail@geometrics.io?subject=Please Add a Country" className="clickable link">email</a> us. If we get enough interest, we'll see what we can do. If you are a developer and would like to team up with us, find this open source project on <a href="https://github.com/geometrics-io/welcome-nomad" className="clickable link">Github</a>.</p>
-        </div>
-    )
+export default class  extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false
+        }
+    }
+    additionalInfo() {
+        this.setState({
+            open: !this.state.open
+        });
+    }
+    render() {
+        return (
+            <div id="origin">
+                <p>USA is currently the country of origin. <span className="clickable" onClick={this.additionalInfo.bind(this)}>Learn {this.state.open === false ? 'more' : 'less'}...</span></p>
+                <ReactCSSTransitionGroup
+                    transitionName="example"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={300}>
+                    {this.state.open === true ?
+                        <div>
+                            <p>If you would like another country added, <a href="mailto:mail@geometrics.io?subject=Please Add a Country" target="_blank" className="clickable link">email</a> us. If we get enough interest, we'll see what we can do. If you are a developer and would like to team up with us, find this open source project on <a href="https://github.com/geometrics-io/welcome-nomad" target="_blank" className="clickable link">Github</a>.</p>
+                        </div>
+                        : ''}
+                </ReactCSSTransitionGroup>
+            </div>
+        );
+    }
 }
